691c28822038dcb15f6fe20d08c33051e0ca43b2
“Will <wrharrison94@gmail.com>
2017-07-05 11:44:34 -0500

    redo

--- server.js
+++ server.js
@@ -17,6 +17,10 @@ app.get('/pair-test', function(request, response) {
   console.log('route succesfully getting hit');
 });
 
+var u1PublicData = [{
+
+}]
+
 app.get('/u1PublicData', function(request, response) {  
   response.send('Hello from Express!');
   console.log('route succesfully getting hit');
